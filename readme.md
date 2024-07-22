4820fe5abb0c35bcce9c1313b136666597d09c70
Andrew Baxter <>
2024-07-22 23:28:34 +0900

    Initial commit

---
+++ readme.md
@@ -0,0 +1,11 @@
+# Flowcontrol
+
+- [Documentation](https://docs.rs/flowcontrol)
+
+General purpose macros and utilities for expediting and clarifying control flow in rust.
+
+- `shed!` - stop `break`s
+
+- `superif!` - like if with multi-statement, branching conditionals
+
+- `ta_return!` - type assert a block's return value (ex: in async blocks, closures)
---
+++ src/lib.rs
@@ -0,0 +1,126 @@
+/// A break shed. This defines a stopping point for `break` in the enclosed code,
+/// as if you had wrapped it in `loop {}`.
+///
+/// Use like:
+///
+/// ```rust
+/// shed!{
+///     ...
+///     break;
+/// }
+/// ```
+///
+/// or, labeled:
+///
+/// ```rust
+/// shed!{
+///     'x _;
+///     ...
+///     break 'x;
+/// }
+/// ```
+#[macro_export]
+macro_rules! shed{
+    // With a break label, like `bb!{ 'label _; ... }`
+    ($l: lifetime _; $($t: tt) *) => {
+        $l: loop {
+            #[allow(unreachable_code)] break {
+                $($t) *
+            };
+        }
+    };
+    // No label, `bb!{ ... }`
+    ($($t: tt) *) => {
+        loop {
+            #[allow(unreachable_code)] break {
+                $($t) *
+            };
+        }
+    };
+}
+
+/// Execute the second block where the first block breaks with the label, like the
+/// first block is a giant `if` condition.
+///
+/// Use like:
+///
+/// ```rust
+/// superif!({
+///     ...
+///     break 'then;
+/// } 'then {
+///     ...
+/// });
+/// ```
+///
+/// If the first block exits without breaking to the label the 2nd block won't be
+/// entered.
+///
+/// There's an alternative form if you want to pass data from the conditional block:
+///
+/// ```rust
+/// superif!({
+///     ...
+///     break 'then 39;
+/// } data = 'then {
+///     println!("{}", data);
+/// });
+/// ```
+#[macro_export]
+macro_rules! superif{
+    ({
+        $($t: tt) *
+    } $l: lifetime {
+        $($t2: tt) *
+    }) => {
+        superif!({
+            $($t) *
+        } _unused = $l {
+            $($t2) *
+        })
+    };
+    ({
+        $($t: tt) *
+    } $i: ident = $l: lifetime {
+        $($t2: tt) *
+    }) => {
+        'superif: loop {
+            let $i = $l: loop {
+                // Double loop to catch unlabeled break
+                #[allow(unreachable_code)] break 'superif(loop {
+                    break 'superif({
+                        $($t) *
+                    });
+                })
+            };
+            $($t2) *
+        }
+    }
+}
+
+#[test]
+fn test_superif() {
+    superif!({
+        if true {
+            break 'then;
+        }
+    } 'then {
+    });
+}
+
+/// Type-assert return type: explicitly communicate the
+/// `Ok`` block return type to the compiler via unreachable code. This relies on a single-argument `Result`
+/// type being available.
+///
+/// Use like `ta_return!(i32);`
+#[macro_export]
+macro_rules! ta_return{
+    ($t: ty) => {
+        if false {
+            fn unreachable_value<T>() -> T {
+                panic!();
+            }
+            return std:: result:: Result::< $t >:: Ok(unreachable_value());
+        }
+    }
+}
